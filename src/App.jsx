afceebf2fd8b7d3a6b42f3efa8c4a133da00c691
MariamO6274 <ostatishvili@gmail.com>
2024-03-11 16:19:55 -0600

    styles js and main app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import styles from "./style";
 
  const App = () => {
-  return (
-    <div className='bg-primary w-full overflow-hidden'>
-      <h1>Hello Im here</h1>
-    </div>
-  )
+  
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing 
+          CardDeal 
+          Testemonials 
+          Clients 
+          CTA 
+          Footer 
+        </div>
+      </div>
+    </div>;
+  
 }
 
 export default App
