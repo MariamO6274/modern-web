8ed380ea42ea51556e8346c9f7de9a3a93179eee
MariamO6274 <ostatishvili@gmail.com>
2024-03-15 08:35:59 -0600

    comments and get started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
       id="home"
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
+      {/* small text above "20% Discount for 1 month...*/}
       <div
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
@@ -19,12 +20,14 @@ const Hero = () => {
           </p>
         </div>
 
+        {/* Hero text */}
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
+            {/* get started button sapareted */}
             <GetStarted />
           </div>
         </div>
